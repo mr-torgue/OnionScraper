d1449b73ed777b83421a598c76305527d7287c33
Mrf <me+github@folmer.info>
2022-01-15 23:07:20 +1100

    ...

--- onion_scraper/settings.py
+++ onion_scraper/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'onion_scraper.spiders'
 #USER_AGENT = 'onion_scraper (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
